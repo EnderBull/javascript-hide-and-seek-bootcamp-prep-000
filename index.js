9c05dde7098fb5e4180f9e04f83264d79b274a96
Simon A Bull <simonbull1@gmail.com>
2018-08-10 16:46:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+/*----------------------------------*/
 
+function getFirstSelector(selector){
+  
+  return document.querySelector(selector);
+}
+
+/*----------------------------------*/
+
+function nestedTarget(){
+  
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+/*----------------------------------*/
+
+function increaseRankBy(n){
+  
+ const increase = document.getElementById('app').querySelectorAll( 'ul li');
+ 
+ 
+ for (let i = 0; i < increase.length; i++){
+   increase[i].innerHTML = (parseInt(increase[i].innerHTML) + n).toString();
+ }
+ 
+  return increase;
+}
+
+/*----------------------------------*/
+
+function deepestChild(){
+ 
+  const dc = document.getElementById('grand-node').querySelector('div')
+  
+   for (let i = 0; i < dc.length; i++){
+    if (dc[i] === 
+  }
+  }
+ 
+}
