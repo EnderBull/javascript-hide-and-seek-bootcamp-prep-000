9f2ab1eb72ecf1172da6d0dc90d9b9249ad5367d
Simon A Bull <simonbull1@gmail.com>
2018-08-10 17:03:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,12 +38,5 @@ function deepestChild(){
   }
   return test;
 }
-function deepestChild() {
-  var lis=document.getElementById("grand-node").querySelectorAll("div");
-  var test;
-  for(let i=0;i<lis.length-1;i++) {
-    test=lis[i].querySelector("div");
-  }
-return test;
-}
+
 
